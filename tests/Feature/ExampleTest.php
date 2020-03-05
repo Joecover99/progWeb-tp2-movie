6c55bace90e1473434d57892f23338fac485e045
Joecover <Joecatcher@hotmail.com>
2020-03-04 19:56:50 -0500

    Test Class + ideas for tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,32 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+/*
+ //   use RefreshDatabase;
+
+    /**
+     * Test creating a new order.
+     *
+     * @return void
+     
+    public function testCreatingANewOrder()
+    {
+        // Run the DatabaseSeeder...
+        $this->seed();
+
+        // Run a single seeder...
+        $this->seed(OrderStatusesTableSeeder::class);
+
+        // ...
+    }
+    //$this->assertDatabaseHas($table, array $data); Assert that a table in the database contains the given data.
+    //$this->assertDeleted($table, array $data); Assert that the given record has been deleted.
+   /* public function testDatabase()
+{
+    $user = factory(App\User::class)->create();
+
+    // Make call to application...
+
+    $this->assertDeleted($user);
+}*/
 }
--- tests/Feature/MovieTest.php
+++ tests/Feature/MovieTest.php
@@ -19,4 +19,37 @@ class MovieTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testBasicExample()
+    {
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->json('POST', '/user', ['name' => 'Sally']);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'created' => true,
+            ]);
+    }
+
+    /*
+    create 
+    rating enum
+    specialFeature
+    fillable
+    language
+    reviews
+    actors
+    hasActors
+    addActor
+    Authentification
+
+    MOVIE
+        Load reviews ONLY IF USER(with a review / without a review)
+        ShowActors (actors in a specific movie movie/actor)
+        Update ONLY IF ADMIN(edit title / edit director / edit actors / edit runtime / edit genre / not admin try to modify = ERROR)
+        Destroy  ONLY IF ADMIN(Remove a movie)
+
+    */
 }
